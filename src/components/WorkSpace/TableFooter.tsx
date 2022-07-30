6c5945332683a699410ea28a9ae72584616d8d4e
GcefLeeWang <Lee.Wang@grapecity.com>
2022-07-30 16:30:57 +0800

    add pagination for user list

--- src/components/WorkSpace/TableFooter.tsx
+++ src/components/WorkSpace/TableFooter.tsx
@@ -1,11 +1,46 @@
-import  React  from "react";
+import React from "react";
 import { connect } from "react-redux";
-import {UserState} from '../../userStore';
+import { userActionCreators, UserState } from '../../userStore';
+import { pagination, GetSequence } from '../utils';
 
-class TablefooterBase extends React.PureComponent<UserState>{
+const DISPLAY_PAGE_COUNT = 10;
+
+/* <li className="page-item" aria-current="page">
+    <a className="page-link text-center page-number active-page" href="#">1</a>
+</li>
+<li className="page-item">
+    <a className="page-link text-center page-number" href="#">2</a>
+</li> */
+
+class TablefooterBase extends React.PureComponent<UserState & { dispatch: any }>{
+
+    fixPageIndex = (pageIndex: number, pageCount: number): number => {
+        if (pageIndex < 1) {
+            pageIndex = 1;
+        }
+        if (pageIndex > pageCount) {
+            pageIndex = pageCount;
+        }
+        return pageIndex;
+    }
+
+    goToPage = (index: number): void => {
+        const { dispatch, pageSize } = this.props;
+        dispatch(userActionCreators.getUserList(index, pageSize));
+    }
+
     render() {
-        const { pageIndex, users, pageSize, userCount } = this.props;
+        const { users, pageSize, userCount } = this.props;
+        const pageCount = Math.ceil(userCount / pageSize);
+
+        let { pageIndex } = this.props;
+        pageIndex = this.fixPageIndex(pageIndex, pageCount);
+
         const startIndex = (pageIndex - 1) * pageSize + 1;
+
+        const { minPage, maxPage } = pagination(pageCount, DISPLAY_PAGE_COUNT, pageIndex);
+        const pages = GetSequence(minPage, maxPage);
+        console.log(maxPage,pageCount);
         return (
             <div className="card-footer shadow-sm d-flex justify-content-between align-items-center grid-footer">
                 <div>
@@ -15,16 +50,27 @@ class TablefooterBase extends React.PureComponent<UserState>{
                 </div>
                 <div className="">
                     <ul className="pagination">
-                        <li className="page-item disabled ">
+                        <li key="prev" className={`page-item ${minPage === 1 ? 'disabled' : ''}`} onClick={() => minPage !== 1 && this.goToPage(pageIndex - 1)}>
                             <a className="page-link border-0">Prev</a>
                         </li>
-                        <li className="page-item" aria-current="page">
-                            <a className="page-link text-center page-number active-page" href="#">1</a>
-                        </li>
-                        <li className="page-item">
-                            <a className="page-link text-center page-number" href="#">2</a>
-                        </li>
-                        <li className="page-item disabled">
+                        {
+                            minPage !== 1 && <li key="more-prev" className="page-item" aria-current="page" onClick={() => this.goToPage(this.fixPageIndex(pageIndex - DISPLAY_PAGE_COUNT, pageCount))}>
+                                <a className="page-link text-center page-number" href="#">...</a>
+                            </li>
+                        }
+                        {
+                            pages.map(p => (
+                                <li key={`page-${p}`} className="page-item" aria-current="page" onClick={() => this.goToPage(p)}>
+                                    <a className={`page-link text-center page-number ${p === pageIndex ? 'active-page' : ''}`} href="#">{p}</a>
+                                </li>
+                            ))
+                        }
+                        {
+                            maxPage !== pageCount && <li key="more-next" className="page-item" aria-current="page" onClick={() => this.goToPage(this.fixPageIndex(pageIndex + DISPLAY_PAGE_COUNT, pageCount))}>
+                                <a className="page-link text-center page-number" href="#">...</a>
+                            </li>
+                        }
+                        <li key="next" className={`page-item ${maxPage === pageCount ? 'disabled' : ''}`}  onClick={() => maxPage !== pageCount && this.goToPage(pageIndex + 1)}>
                             <a className="page-link border-0" href="#">Next</a>
                         </li>
                     </ul>
@@ -34,6 +80,6 @@ class TablefooterBase extends React.PureComponent<UserState>{
 };
 
 
-const  Tablefooter = connect(state=>state)(TablefooterBase) as React.ComponentClass<{}>;
+const Tablefooter = connect(state => state)(TablefooterBase) as React.ComponentClass<{}>;
 
-export default  Tablefooter;
+export default Tablefooter;
---
+++ src/components/utils.ts
@@ -0,0 +1,46 @@
+
+export interface PaginationInfor {
+    minPage: number;
+    maxPage: number;
+}
+
+export function pagination(pageCount: number, displayPageCount: number, pageIndex: number): PaginationInfor {
+    if (pageIndex < 1) {
+        pageIndex = 1;
+    }
+    if (pageIndex > pageCount) {
+        pageIndex = pageCount;
+    }
+
+    let minPage = pageIndex - Math.floor(displayPageCount / 2);
+    if (minPage < 1) {
+        minPage = 1;
+    }
+    let maxPage = minPage + displayPageCount - 1;
+    if (maxPage > pageCount) {
+        maxPage = pageCount;
+    }
+
+    if(maxPage === pageCount)
+    {
+        minPage = maxPage - displayPageCount + 1;
+        if (minPage < 1) {
+            minPage = 1;
+        }
+    }
+
+    return {
+        minPage,
+        maxPage,
+    };
+}
+
+export function GetSequence(minPage: number, maxPage: number)
+{
+    const numbers =[];
+    for(let i=minPage;i<=maxPage;i++)
+    {
+        numbers.push(i);
+    }
+    return numbers;
+}
