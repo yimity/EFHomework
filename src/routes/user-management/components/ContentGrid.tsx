40f829781fbc71b14c6e939069287865c94a3dcd
Lance Zhao <lance.zhao@grapecity.com>
2022-08-02 09:40:54 +0800

    delete origin data

--- src/routes/user-management/components/ContentGrid.tsx
+++ src/routes/user-management/components/ContentGrid.tsx
@@ -11,16 +11,6 @@ interface ConnectedProps {
 	dispatch?: any;
 }
 
-// const users: User[] =
-//   [
-//     { id: 1, name: 'Amanda Harvey', email: 'amanda@site.com', photo: '', position: 'Director', department: 'Human resources', country: 'United Kingdom', status: 'Active', portfolio: 72, role: 'Employee' },
-//     { id: 2, name: 'Anne Richard', email: 'anne@site.com', photo: 'https://github.com/mdo.png', position: 'Seller', department: 'Branding products', country: 'United States', status: 'Active', portfolio: 24, role: 'Employee' },
-//     { id: 3, name: 'Bran Hooligan', email: 'bran@site.com', photo: '', position: 'Director', department: 'Accounting', country: 'France', status: 'Active', portfolio: 71, role: 'Employee' },
-//     { id: 4, name: 'Chris Mathew', email: 'chris@site.com', photo: '', position: 'Developer', department: 'Mobile App', country: 'Switzerland', status: 'Pending', portfolio: 0, role: 'Employee' },
-//     { id: 5, name: 'Clarice Boone', email: 'clarice@site.com', photo: 'https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg', position: 'Seller', department: 'Branding products', country: 'United Kingdom', status: 'Active', portfolio: 37, role: 'Employee' },
-//     { id: 6, name: 'Lewis Clarke', email: 'lewis@site.com', photo: '', position: 'Co-founder', department: 'IT department', country: 'Switzerland', status: 'Pending', portfolio: 100, role: 'Owner' },
-//   ];
-
 const gridHeaders = [
   { name: 'NAME', colWidth: 3 },
   { name: 'POSITION', colWidth: 2 },
